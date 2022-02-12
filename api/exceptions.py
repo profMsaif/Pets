2c4b13f771a762e819e418bf7419a54ff832a7f4
profMsaif <profmsaif@gmail.com>
2022-02-12 22:41:31 +0500

    ADD# UnAuthorizedAccess exception

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,2 +1,12 @@
+from  rest_framework import exceptions, status
+from django.utils.translation import gettext_lazy as _
+
+
 class FileNameWithoutExtension(Exception):
     ...
+
+
+class UnAuthorizedAccess(exceptions.APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = _("Incorrect API KEY.")
+    default_code = "Unauthorized"
