0f5a8afc6d02d0a1da4428289d981a3669f3494a
profMsaif <profmsaif@gmail.com>
2022-02-12 17:29:38 +0500

    ADD# create models and make make migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+# we set some choices  
+PETS_TYPE_CHOICES = (("cat","cat"), ("dog","dog"))
+PETS_SEX_CHOICES = (("boy","boy"), ("girl","girl"))
+
+# pets photo model
+class PetsPhoto(models.Model):
+    # the pet object must be created before adding the photo
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    pet = models.ForeignKey(to="Pets", on_delete=models.CASCADE)
+    url = models.URLField(max_length=300, null=False)
+
+# this can be optimized  by adding the type and the name to
+# a seperated table linking them through foreign key instead of choices
+class Pets(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=20, choices=PETS_SEX_CHOICES)
+    age = models.IntegerField()
+    type = models.CharField(max_length=20, choices = PETS_TYPE_CHOICES)
+    photos = models.ManyToManyField(PetsPhoto, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
