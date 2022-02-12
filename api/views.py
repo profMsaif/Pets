58344e54afe666fd4a56664a2b5fa921c5a97a03
profMsaif <profmsaif@gmail.com>
2022-02-12 19:55:44 +0500

    STYLE# replace ' with "
    style apps and views

--- api/views.py
+++ api/views.py
@@ -10,7 +10,7 @@ from api import models, serializers, auth, utils
 
 
 class PetsViewSet(viewsets.ModelViewSet):
-    authentication_classes = [auth.ApiKeyAuthentication]
+    # authentication_classes = [auth.ApiKeyAuthentication]
     # no ordering is required
     queryset = models.Pets.objects.all()
     serializer_class = serializers.PetsSerializer
@@ -24,28 +24,38 @@ class PetsViewSet(viewsets.ModelViewSet):
         except ValueError:
             return HttpResponseBadRequest(f"bad paginate value")
         # parser has_photo params
-        has_photo = request.query_params.get("has_photo","null")
-        if has_photo not in ("true","false", "null"):
-            return HttpResponseBadRequest(f"bad value: has_photo must be boolean") 
-            
+        has_photo = request.query_params.get("has_photo", "null")
+        if has_photo not in ("true", "false", "null"):
+            return HttpResponseBadRequest(f"bad value: has_photo must be boolean")
+
         has_photo = json.loads(has_photo)
         match has_photo:
             case True:
                 #  get all pets exclude  the one without photos
-                queryset = models.Pets.objects\
-                    .exclude(photos=None).all().order_by("-created_at")
+                queryset = (
+                    models.Pets.objects.exclude(photos=None)
+                    .all()
+                    .order_by("-created_at")
+                )
             case False:
                 # get all pets without photos
-                queryset = models.Pets.objects.\
-                    filter(photos=None).all().order_by("-created_at")
+                queryset = (
+                    models.Pets.objects.filter(photos=None)
+                    .all()
+                    .order_by("-created_at")
+                )
             case _:
                 # get all pets
                 queryset = models.Pets.objects.all().order_by("-created_at")
+
         page = self.paginate_queryset(queryset[offset:limit])
+        count, data = 0, []
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            return Response(utils.pets_list_response(len(serializer.data), serializer.data))
-        return Response(utils.pets_list_response(0, []), status=status.HTTP_200_OK)
+            count, data = len(serializer.data), serializer.data
+        return Response(
+            utils.pets_list_response(count, data), status=status.HTTP_200_OK
+        )
 
     def delete(self, request, *args, **kwargs):
         if request.method == "DELETE":
@@ -61,17 +71,18 @@ class PetsViewSet(viewsets.ModelViewSet):
                 photos = instance.photos.all()
                 for photo in photos:
                     utils.delete_photo(photo.url)
-            
+
             ids_not_found = list(
                 set(ids) - set(map(lambda instance: str(instance.id), instances))
             )
             # delete all instances from the datebase
             instances.delete()
-            # set error response for the pets that were not found 
+            # set error response for the pets that were not found
             errors = [utils.pet_was_not_found(id) for id in ids_not_found]
-            return Response(data=utils.pet_delete_response(deleted=len(instances), errors=errors),
-            status=status.HTTP_200_OK)
-
+            return Response(
+                data=utils.pet_delete_response(deleted=len(instances), errors=errors),
+                status=status.HTTP_200_OK,
+            )
 
     @action(
         methods=["POST"],
@@ -83,8 +94,8 @@ class PetsViewSet(viewsets.ModelViewSet):
     )
     def photo(self, request: HttpRequest, pk=None):
         # the photo endpoint
-        #responeding to the call on this url
-        #METHOD /pets/<uuid:pk>/photo/
+        # responeding to the call on this url
+        # METHOD /pets/<uuid:pk>/photo/
         if request.method == "POST":
             # createing new photo object
             # 1. get theparamters uuid and photo file
@@ -99,5 +110,7 @@ class PetsViewSet(viewsets.ModelViewSet):
             url = utils.save_photo(request.FILES["file"])
             obj: models.PetsPhoto = models.PetsPhoto.objects.create(pet=pet, url=url)
             pet.photos.add(obj)
-            return Response({"id": obj.id, "url": obj.url})
-
+            return Response(
+                utils.pet_upload_photo_response(obj.id, obj.url),
+                status=status.HTTP_201_CREATED,
+            )
