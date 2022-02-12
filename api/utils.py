9022392ba92cbc597d50afd7ab7ce220446dcb30
profMsaif <profmsaif@gmail.com>
2022-02-12 18:35:31 +0500

    ADD# utils and exceptions

---
+++ api/utils.py
@@ -0,0 +1,63 @@
+"""
+in this api we are going  to save photos
+delete them and perform other operation that 
+are not part of the class 
+this file is going to separate them
+providing convent way to work.
+"""
+import uuid
+import pathlib
+from typing import Dict
+from pets import settings
+from api import exceptions
+
+
+def pet_was_not_found(pet_id: str) -> Dict[str, str]:
+    return {"id": pet_id, "error": "Pet with the matching ID was not found."}
+
+
+def generate_photo_file_name(extension: str) -> str:
+    """return random photo name"""
+    return f"{uuid.uuid4()}.{extension}"
+
+
+def create_photo_url(photo_file_name) -> str:
+    return f"{settings.DOMAIN_URL}/{photo_file_name}"
+
+
+def get_photo_path_from_url(url: str) -> str:
+    # address format should be a domain like example.com
+    # or ip:port
+    # domain format https://address
+    # url format https://address/filename.extension
+    # get the file name in this format /filename.extension
+    filename = url.split(settings.DOMAIN_URL)[-1]
+    return f"{settings.MEDIA_ROOT}{filename}"
+
+
+def save_photo(photo) -> str:
+    """take photofile save and return the url of the file"""
+    # the photo must have an extension in the filename
+    original_file_name = photo.name.split(".")
+    if len(original_file_name) != 2:
+        return exceptions.FileNameWithoutExtension("bad image file name")
+    extension = original_file_name[1]
+    photo_file_name = generate_photo_file_name(extension)
+    # write photo by chunks
+    with open(f"{settings.MEDIA_ROOT}/{photo_file_name}", "wb+") as fb:
+        for chunk in photo.chunks():
+            fb.write(chunk)
+    return create_photo_url(photo_file_name)
+
+
+def delete_photo(url) -> None:
+    """remove a photo by url"""
+    photo = get_photo_path_from_url(url)
+    try:
+        pathlib.Path(photo).unlink()
+    except FileNotFoundError:
+        # we are deleting a file that has been already deleted
+        # this happend if different pets have the same image
+        # which shouldn't happen ever
+        # log this
+        pass
